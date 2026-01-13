41ab1455bb051daf110face2a0640a3f7089d430
Guillaume Everarts de Velp <edvgui@gmail.com>
2026-01-13 21:51:20 +0100

    Add tests

--- inmanta_plugins/sops/__init__.py
+++ inmanta_plugins/sops/__init__.py
@@ -17,10 +17,15 @@ Contact: edvgui@gmail.com
 """
 
 import json
+import os
+import pathlib
+import platform
+import re
 import subprocess
 from dataclasses import dataclass
 
 import pydantic
+import requests
 
 from inmanta.agent.handler import LoggerABC
 from inmanta.plugins import plugin
@@ -35,6 +40,90 @@ class SopsBinary:
     version: str
 
 
+def find_sops_in_path(binary_name: str = "sops") -> SopsBinary:
+    """
+    Try to find sops in the current file system, which exploring the PATH
+    environment variable.
+
+    :param binary_name: The name for the binary containing the sops tool.
+    """
+    PATH = os.environ["PATH"].split(":")
+
+    for folder in PATH:
+        if (path := pathlib.Path(folder) / binary_name).exists():
+            try:
+                # Found a sops binary, execute it to resolve the version
+                version_output = subprocess.check_output([str(path), "-v"], text=True)
+            except subprocess.CalledProcessError as e:
+                raise RuntimeError(
+                    f"Invalid binary {path}, it doesn't recognize flag -v"
+                ) from e
+
+            # Try to parse the output version
+            matched = re.match(r"^sops (\d+\.\d+\.\d+)[^\d]", version_output)
+            if not matched:
+                raise RuntimeError(
+                    f"Unexpected version format output for binary {path}: {version_output}"
+                )
+
+            return SopsBinary(
+                path=str(path),
+                args=[],
+                version=matched.group(1),
+            )
+
+    raise LookupError(f"Failed to find any binary named {binary_name} in PATH {PATH}")
+
+
+def install_sops_from_github(
+    path: pathlib.Path,
+    version: str = "3.11.0",
+) -> SopsBinary:
+    """
+    Install a binary at the given path.  No file must exist at the path location.
+    The binary is downloaded from github.
+
+    :param path: The path at which the sops binary should be created.
+    :param version: The version to download from github.
+    """
+    if path.exists():
+        raise RuntimeError(f"A file already exist at path {path}")
+
+    # Make sure the parent directory exists
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    # Figure out the architecture of the current platform
+    architecture = {
+        "x86_64": "amd64",
+        "aarch64": "arm64",
+    }[platform.machine()]
+
+    # Define the desired sops path
+    sops = SopsBinary(
+        path=str(path),
+        args=[],
+        version=version,
+    )
+
+    # Download the sops binary and place it in the temporary folder
+    with requests.get(
+        (
+            "https://github.com/getsops/sops/releases/download/"
+            f"v{sops.version}/sops-v{sops.version}.linux.{architecture}"
+        ),
+        stream=True,
+    ) as r:
+        r.raise_for_status()
+        with open(sops.path, "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+
+    # Make sure that the sops binary is executable
+    pathlib.Path(sops.path).chmod(755)
+
+    return sops
+
+
 @reference("sops::DecryptedFileReference")
 class DecryptedFileReference(Reference[dict]):
     """
@@ -46,17 +135,29 @@ class DecryptedFileReference(Reference[dict]):
         self,
         binary: SopsBinary | Reference[SopsBinary],
         encrypted_file: str | Reference[str],
+        encrypted_file_type: str | Reference[str],
     ):
+        super().__init__()
         self.binary = binary
         self.encrypted_file = encrypted_file
+        self.encrypted_file_type = encrypted_file_type
 
     def resolve(self, logger: LoggerABC) -> dict:
         binary = self.resolve_other(self.binary, logger)
         encrypted_file = self.resolve_other(self.encrypted_file, logger)
+        encrypted_file_type = self.resolve_other(self.encrypted_file_type, logger)
 
         # Run existing binary to decrypt the file
         output = subprocess.check_output(
-            [binary.path, "decrypt", *binary.args, "--output-type", "json"],
+            [
+                binary.path,
+                "decrypt",
+                *binary.args,
+                "--input-type",
+                encrypted_file_type,
+                "--output-type",
+                "json",
+            ],
             input=encrypted_file,
             text=True,
         )
@@ -70,8 +171,9 @@ class DecryptedFileReference(Reference[dict]):
 def create_decrypted_file_reference(
     binary: SopsBinary | Reference[SopsBinary],
     encrypted_file: str | Reference[str],
+    encrypted_file_type: str | Reference[str],
 ) -> DecryptedFileReference:
-    return DecryptedFileReference(binary, encrypted_file)
+    return DecryptedFileReference(binary, encrypted_file, encrypted_file_type)
 
 
 @reference("sops::DecryptedValueReference")
@@ -81,6 +183,7 @@ class DecryptedValueReference(Reference[str]):
         decrypted_file: dict | Reference[dict],
         value_path: str | Reference[str],
     ):
+        super().__init__()
         self.decrypted_file = decrypted_file
         self.value_path = value_path
 
@@ -94,5 +197,5 @@ class DecryptedValueReference(Reference[str]):
 def create_decrypted_value_reference(
     decrypted_file: dict | Reference[dict],
     value_path: str | Reference[str],
-) -> str:
+) -> DecryptedValueReference:
     return DecryptedValueReference(decrypted_file, value_path)
